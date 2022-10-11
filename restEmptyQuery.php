44da330a56a079219afac6ab74bc6093767fe877
nikolayShvetsov <76684191+nikolayShvetsov@users.noreply.github.com>
2022-10-11 11:37:35 +0300

    create restEmptyQuery.php

---
+++ restEmptyQuery.php
@@ -0,0 +1,21 @@
+<?
+
+$method = 'method';
+
+$queryUrl = 'https://'.$_REQUEST['DOMAIN'].'/rest/'.$method.'.json';
+$queryData = http_build_query(array_merge($params, array("auth" => $_REQUEST['AUTH_ID'])));
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $queryUrl,
+    CURLOPT_POSTFIELDS => $queryData,
+));
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+echo  var_dump($result);
